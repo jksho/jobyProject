d0cd617bf464c04a49b2936eec52c5ddb7234ffa
Jimmy Ho <jimmy.ho@wer.ai>
2022-07-13 23:18:45 -0700

    fix test_unit_ip_scanner

--- test_unit_ip_scanner.py
+++ test_unit_ip_scanner.py
@@ -1,16 +1,19 @@
 import unittest
-import ping3
-from util import FileUtil, PingUtil
-from parallel.SequentialPing import checkAllIpsSequentially
-from parallel.MultiThreadingPing import checkAllIpsByMultiThreading
-from parallel.MulitprocessingPing import checkAllIpsByMultiprocessing
+#import ping3
+import util import FileUtil, PingUtil
 
 def test_ping(self):
     test_ipAddress = "172.16.1.1"
-    ping3.EXCEPTIONS = True
-    try:
-        ping3.ping("not.exist.com")
-    except ping3.errors.HostUnknown:  # Specific error is catched.
-        print("Host unknown error raised.")
-    except ping3.errors.PingError:  # All ping3 errors are subclasses of `PingError`.
-        print("A ping error raised.")
+     flag = PingUtil.isIPReachable(ip);
+    if flag:
+        formattedOut = "{}: pingable".format(ip)
+    else:
+        formattedOut = "{}: not pingable".format(ip)
+    return(formattedOut)
+    # ping3.EXCEPTIONS = True
+    # try:
+    #     ping3.ping(test_ipAddress)
+    # except ping3.errors.HostUnknown:  # Specific error is catched.
+    #     print("Host unknown error raised.")
+    # except ping3.errors.PingError:  # All ping3 errors are subclasses of `PingError`.
+    #     print("A ping error raised.")
