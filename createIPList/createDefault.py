51d3baf77730029c3eb4e8ab7b8bc8a89b25377e
Jimmy Kin Shing Ho <mkssterling@gmail.com>
2022-07-13 22:08:51 -0700

    first octet typo should be 172 not 171

--- createIPList/createDefault.py
+++ createIPList/createDefault.py
@@ -2,11 +2,11 @@ def generateIPList(fileName):
     ip_addr_range_1 = input ('first ip range to scan x.y.z first 3 octet (default is 172.16.1)\n')
     if not ip_addr_range_1:
         # Default values for subnet #1
-        ip_addr_range_1 = "171.16.1"    
+        ip_addr_range_1 = "172.16.1"    
     ip_addr_range_2 = input ('second ip range to scan x.y.z first 3 octet (default is 172.16.2)\n')
     if not ip_addr_range_2:
         # Default values for subnet #2
-        ip_addr_range_2 = "171.16.2"    
+        ip_addr_range_2 = "172.16.2"    
     excluded_octet = input('What last octet do you want to exclude in the scan? 1 to 254 (default is 1)\n')
     if not excluded_octet:
         # Default values for final octet
