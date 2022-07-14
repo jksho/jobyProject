aed4d528997cb64a65934e6885e030e803f5e16b
Jimmy Kin Shing Ho <mkssterling@gmail.com>
2022-07-13 22:00:43 -0700

    Tested createIPList()

--- Main3.py
+++ Main3.py
@@ -5,7 +5,7 @@ from parallel.SequentialPing import checkAllIpsSequentially
 from parallel.MultiThreadingPing import checkAllIpsByMultiThreading
 from parallel.MulitprocessingPing import checkAllIpsByMultiprocessing
 
-FILE_NAME = "ipList/ipaddress-generated.txt"
+FILE_NAME = "IPList/ipaddress-generated.txt"
 ip_addr_range_1 = "171.16.1"    # Default values for subnet #1
 ip_addr_range_2 = "171.16.2"    # Default values for subnet #2
 excluded_octet = "1"            # Default value for octet to exclude
--- createIPList/createDefault.py
+++ createIPList/createDefault.py
@@ -1,13 +1,16 @@
-def generateIPList(fileName)
+def generateIPList(fileName):
     ip_addr_range_1 = input ('first ip range to scan x.y.z first 3 octet (default is 172.16.1)\n')
     if not ip_addr_range_1:
-        ip_addr_range_1 = "171.16.1"    # Default values for subnet #1
+        # Default values for subnet #1
+        ip_addr_range_1 = "171.16.1"    
     ip_addr_range_2 = input ('second ip range to scan x.y.z first 3 octet (default is 172.16.2)\n')
     if not ip_addr_range_2:
-        ip_addr_range_2 = "171.16.2"    # Default values for subnet #1
+        # Default values for subnet #2
+        ip_addr_range_2 = "171.16.2"    
     excluded_octet = input('What last octet do you want to exclude in the scan? 1 to 254\ (default is 1)\n')
     if not excluded_octet:
-        excluded_octet = "1"    # Default values for final octet
+        # Default values for final octet
+        excluded_octet = "1"    
     ignore = [0,int(excluded_octet)]
     print ("excluding " + ip_addr_range_1 + "." + excluded_octet)
     print ("excluding " + ip_addr_range_2 + "." + excluded_octet)
@@ -16,4 +19,5 @@ def generateIPList(fileName)
     for ip_last_octet in scan_list:
         f.write("{}.{}\n".format(ip_addr_range_1, ip_last_octet))
         f.write("{}.{}\n".format(ip_addr_range_2, ip_last_octet))
-    f.close()  # Close the created file
+    # Close the created file
+    f.close()
