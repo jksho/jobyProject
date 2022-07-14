d4ae8789d235d1943a5d4e8c6c0ed1b0ad5e8599
Jimmy Ho <jimmy.ho@wer.ai>
2022-07-14 01:06:25 -0700

    chage the name of the function to call unittest

--- test_unit_ip_scanner.py
+++ test_unit_ip_scanner.py
@@ -1,14 +1,13 @@
 import unittest
 from util import FileUtil, PingUtil
 
-class TestSum(unittest.TestCase):
+class TestpingIP(unittest.TestCase):
     def test_ping(self):
         """
         Test that it can ping 172.16.1.1 (gateway)
         """
         test_ipAddress = "172.16.1.1"
         result = PingUtil.isIPReachable(test_ipAddress);
-        print(result)
         self.assertEqual(result,True)
         
 if __name__ == '__main__':
